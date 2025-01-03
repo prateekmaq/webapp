af330b1657a096d55692ad118190cb6f78f2aa51
prateekmaq <prateekl@maqsoftware.com>
2025-01-03 16:47:01 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,13 +1,39 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
+import os
 import requests
+from dotenv import load_dotenv
+
+# Load environment variables from .env file
+load_dotenv()
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
+api_url = os.getenv('API_URL')
+
+def fetch_users(num_users=30):
+    url = f"{api_url}?results={num_users}"
+    response = requests.get(url)
+    return response.json()['results']
 
 @app.route('/')
-def home():
-    response = requests.get('https://randomuser.me/api/?results=30')
-    users = response.json()['results']
-    return render_template('index.html', users=users)
+def index():
+    return render_template('index.html')
+
+@app.route('/api/users')
+def get_users():
+    users = fetch_users()
+    return jsonify(users)
+
+@app.route('/env')
+def display_env():
+    env_variables = {
+        "FLASK_APP_NAME": os.getenv('FLASK_APP_NAME'),
+        "FLASK_ENV": os.getenv('FLASK_ENV'),
+        "FLASK_DEBUG": os.getenv('FLASK_DEBUG'),
+        "API_URL": os.getenv('API_URL'),
+        "SECRET_KEY": os.getenv('SECRET_KEY')
+    }
+    return render_template('env.html', env_variables=env_variables)
 
 if __name__ == '__main__':
     app.run(debug=True)
